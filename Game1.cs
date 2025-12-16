983ebd201feb9f699042cd9cf410bc4d648586bf
drago <drago@EM-LOPTAP>
2025-12-16 09:49:57 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_7___Collisions_with_Rectangles
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        KeyboardState keyboardState;
+        MouseState mouseState;
+
+        Texture2D pacLeftTexture;
+        Texture2D pacRightTexture;
+        Texture2D pacUpTexture;
+        Texture2D pacDownTexture;
+        Texture2D currentPacTexture;
+        Rectangle pacRect;
+        Vector2 pacSpeed;
+
+        Texture2D exitTexture;
+        Rectangle exitRect;
+
+        Texture2D barrierTexture;
+        Rectangle barrierRect1, barrierRect2;
+
+        Texture2D coinTexture;
+        Rectangle coinRect;
+
+        Rectangle window;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            pacSpeed = Vector2.Zero;
+            pacRect = new Rectangle(10, 10, 60, 60);
+            barrierRect1 = new Rectangle(0, 250, 350, 75);
+            barrierRect2 = new Rectangle(450, 250, 350, 75);
+            coinRect = new Rectangle(400, 50, coinTexture.Width, coinTexture.Height);
+            exitRect = new Rectangle(700, 380, 100, 100);
+            window = new Rectangle(0, 0, 800, 480);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            currentPacTexture = pacRightTexture;
+
+            barrierTexture = Content.Load<Texture2D>("rock_barrier");
+            
+            exitTexture = Content.Load<Texture2D>("hobbit_door");
+            
+            coinTexture = Content.Load<Texture2D>("coin");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            keyboardState = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            pacSpeed = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacSpeed.X -= 2;
+                currentPacTexture = pacLeftTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacSpeed.X += 2;
+                currentPacTexture = pacRightTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacSpeed.Y -= 2;
+                currentPacTexture = pacUpTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacSpeed.Y += 2;
+                currentPacTexture = pacDownTexture;
+            }
+            pacRect.Offset(pacSpeed);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(barrierTexture, barrierRect1, Color.White);
+            _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+            _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+            _spriteBatch.Draw(coinTexture, coinRect, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Monogame_7___Collisions_with_Rectangles.Game1();
+game.Run();
